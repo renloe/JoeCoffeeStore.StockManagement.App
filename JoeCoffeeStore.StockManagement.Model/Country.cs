3c3fbbe539d6ebb79bbbe09ec22194eeee6199d2
renloe <renloe@live.com>
2015-11-22 16:22:53 -0500

    Added Coffee class

---
+++ JoeCoffeeStore.StockManagement.Model/Country.cs
@@ -0,0 +1,17 @@
+﻿namespace JoeCoffeeStore.StockManagement.Model
+{
+    public enum Country
+    {
+        Brasil,
+        Ecuador,
+        Ethiopia,
+        Vietnam,
+        Columbia,
+        Peru,
+        Mexico,
+        Honduras,
+        India,
+        Indonesia,
+        Guatemala
+    }
+}
