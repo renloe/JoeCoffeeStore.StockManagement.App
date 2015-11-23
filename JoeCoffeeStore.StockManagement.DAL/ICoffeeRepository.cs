89c9d4fd8eb9c19726d1fadbfc253b0173a9fba2
renloe <renloe@live.com>
2015-11-22 19:03:08 -0500

    Added CoffeeRepository

---
+++ JoeCoffeeStore.StockManagement.DAL/ICoffeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using JoeCoffeeStore.StockManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoeCoffeeStore.StockManagement.DAL
+{
+    public interface ICoffeeRepository
+    {
+        void DeleteCoffee(Coffee coffee);
+        Coffee GetACoffee();
+        Coffee GetCoffeeById(int id);
+        List<Coffee> GetCoffees();
+        void UpdateCoffee(Coffee coffee);
+    }
+}
