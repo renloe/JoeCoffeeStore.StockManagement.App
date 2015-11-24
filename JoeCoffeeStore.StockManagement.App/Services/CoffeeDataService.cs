9605607586b4a10ae788d7f9ac8f278e16a1130a
renloe <renloe@live.com>
2015-11-23 19:39:21 -0500

    Added CoffeDataService and Interface

---
+++ JoeCoffeeStore.StockManagement.App/Services/CoffeeDataService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JoeCoffeeStore.StockManagement.Model;
+using JoeCoffeeStore.StockManagement.DAL;
+
+namespace JoeCoffeeStore.StockManagement.App.Services
+{
+    public class CoffeeDataService : ICoffeeDataService
+    {
+        ICoffeeRepository _repository;
+
+        public CoffeeDataService(ICoffeeRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Coffee GetCoffeeDetail(int coffeeId)
+        {
+            return _repository.GetCoffeeById(coffeeId);
+        }
+
+        public List<Coffee> GetAllCoffees()
+        {
+            return _repository.GetCoffees();
+        }
+
+        public void UpdateCoffee(Coffee coffee)
+        {
+            _repository.UpdateCoffee(coffee);
+        }
+
+        public void DeleteCoffee(Coffee coffee)
+        {
+            _repository.DeleteCoffee(coffee);
+        }
+    }
+}
---
+++ JoeCoffeeStore.StockManagement.App/Services/ICoffeeDataService.cs
@@ -0,0 +1,17 @@
+﻿using JoeCoffeeStore.StockManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoeCoffeeStore.StockManagement.App.Services
+{
+    public interface ICoffeeDataService
+    {
+        void DeleteCoffee(Coffee coffee);
+        List<Coffee> GetAllCoffees();
+        Coffee GetCoffeeDetail(int coffeeId);
+        void UpdateCoffee(Coffee coffee);
+    }
+}
