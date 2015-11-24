597c5999af13396ca38ef9fe8ba70fa463a05e38
renloe <renloe@live.com>
2015-11-23 19:46:45 -0500

    Added Dialog Server and Interface. Added CoffeeDetailView

---
+++ JoeCoffeeStore.StockManagement.App/Services/DialogService.cs
@@ -0,0 +1,35 @@
+﻿using JoeCoffeeStore.StockManagement.App.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace JoeCoffeeStore.StockManagement.App.Services
+{
+    public class DialogService : IDialogService
+    {
+        private Window _coffeeDetailView = null;
+
+        public DialogService()
+        {
+        }
+
+        public void ShowDetailDialog()
+        {
+            _coffeeDetailView = new CoffeeDetailView();
+            _coffeeDetailView.ShowDialog();
+        }
+
+        public void CloseDetailDialog()
+        {
+            if (_coffeeDetailView != null)
+            {
+                _coffeeDetailView.Close();
+            }
+        }
+
+       
+    }
+}
---
+++ JoeCoffeeStore.StockManagement.App/Services/IDialogService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoeCoffeeStore.StockManagement.App.Services
+{
+    public interface IDialogService
+    {
+        void CloseDetailDialog();
+        void ShowDetailDialog();
+    }
+}
